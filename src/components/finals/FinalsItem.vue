f8bf419ff55986a17ad59812cca4138f29d57364
Toshi Stewart <toshi.stewart@gmail.com>
2022-11-09 13:49:43 +0100

    Initial Commit

---
+++ src/components/finals/FinalsItem.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="fixture-item" :class="[{current: fixtureRoundNumber === roundNumber, live: gameStatus === 'live', post: gameStatus === 'post'}]">
+    <div class="fixture-date">
+      <span>
+        <span class="fixture-date-day">{{gameTime(timestamp).day}}</span>
+        <span class="fixture-date-month">{{gameTime(timestamp).month}}</span>
+        <span class="fixture-date-date">{{gameTime(timestamp).date}}</span>
+      </span>
+      <span v-if="gameStatus === 'pre'" class="fixture-date-time">{{gameTime(timestamp).time}}</span>
+      <span v-else-if="gameStatus === 'post'" class="fixture-date-time">FT</span>
+      <span v-else-if="gameStatus === 'live'" class="fixture-date-time">
+        <span class="fixture-live">LIVE </span>
+        {{matchStatus}} {{matchTime}}
+      </span>
+    </div>
+    <FixtureTeam 
+      :kitsource="homeTeamKit(clubData, teamHome.code)"
+      :gameStatus="gameStatus"
+      :teamname="clubData[teamHome.code].name"
+      :goals="teamHome.goals"
+      :behinds="teamHome.behinds"
+      :totalscore="teamHome.score"
+    />
+    <div class="fixture-vs">vs</div>
+    <FixtureTeam 
+      :kitsource="awayTeamKit(clubData, teamHome.code, teamAway.code)"
+      :gameStatus="gameStatus"
+      :teamname="clubData[teamAway.code].name"
+      :goals="teamAway.goals"
+      :behinds="teamAway.behinds"
+      :totalscore="teamAway.score"
+    />
+    <div class="fixture-venue">{{venue}}</div>
+  </div>
+</template>
+<script lang="ts">
+import FixtureTeam from '../fixture/FixtureTeam.vue'
+
+export default {
+    props: {
+      clubData: {
+        type: Object
+      },
+      gameStatus: {
+        type: String
+      },
+      matchStatus: {
+        type: String
+      },
+      matchTime: {
+        type: String
+      },
+      fixtureRoundNumber: {
+        type: Number
+      },
+      roundNumber: {
+        type: Number
+      },
+      timestamp: {
+        type: String
+      },
+      date: {
+        type: String,
+        required: false
+      },
+      day: {
+        type: String,
+        required: false
+      },
+      month: {
+        type: String,
+        required: false
+      },
+      time: {
+        type: String,
+        required: false
+      },
+      venue: {
+        type: String,
+        required: false
+      },
+      teamHome: {
+        type: Object
+      },
+      teamAway: {
+        type: Object
+      }
+    },
+    components: { FixtureTeam },
+    methods: {
+      homeTeamKit(clubData:any, teamkey: any) {
+        return clubData[teamkey].kit.home
+      },
+      awayTeamKit(clubData:any, hometeamkey: any, awayteamkey: any) {
+        if (clubData[awayteamkey].kit.clash.teams.includes(hometeamkey)) {
+          return clubData[awayteamkey].kit.clash.kit
+        } else {
+          return clubData[awayteamkey].kit.away
+        }
+      },
+      gameTime(timestamp:any) {
+        let date = new Date(timestamp);
+        let dayValue = date.getDay();
+        let monthValue = date.getMonth();
+        let dateValue = date.getDate();
+        let hoursValue = date.getHours();
+        let minutesValue = date.getMinutes();
+        let daysOfTheWeek = [
+          "Sunday", 
+          "Monday", 
+          "Tuesday", 
+          "Wednesday", 
+          "Thursday", 
+          "Friday", 
+          "Saturday"
+        ];
+        let monthsOfTheYear = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ];
+        return {
+          day: daysOfTheWeek[dayValue],
+          month: monthsOfTheYear[monthValue],
+          date: dateValue,
+          time: hoursValue + ':' + minutesValue
+        }
+      }
+    }
+}
+</script>
+<style scoped lang="scss">
+  .fixture-item {
+    display: none;
+    @include borderBox();
+    background: $color-grey-dark;
+    color: $color-white;
+    text-align: center;
+
+    &.live {
+      background: $color-white;
+      color: $color-black;
+    }
+
+    &.post {
+      background: #444;
+    }
+    &.current {
+      display: inline-block;
+      margin: 0 17px 17px 0;
+      padding: 0 17px;
+      width: 350px;
+
+      @media screen and (max-width: $break-m) {
+        width: 100%;
+        margin: 0 0 17px;
+      }
+    }
+  }
+
+  .fixture-date {
+    padding: 26px 0 3px;
+    line-height: 1.5;
+    font-weight: 600;
+
+    &-day,
+    &-month,
+    &-date {
+      margin-right: 5px; 
+    }
+
+    &-time {
+      font-weight: 400;
+    }
+  }
+
+  .fixture-live {
+    color: $color-red;
+    font-weight: 600;
+  }
+
+  .fixture-vs {
+    display: inline-block;
+    padding: 0 25px;
+    vertical-align: top;
+    margin-top: 50px;
+  }
+
+  .fixture-venue {
+    padding: 25px 0 30px;
+    font-size: 12px;
+  }
+</style>
---
+++ src/components/fixture/FixtureTeam.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="fixture-team">
+    <IconKit :source="kitsource" :size="161" class="icon-kit" />
+    <span class="fixture-team-name">{{teamname}}</span>
+    <span v-if="gameStatus === 'pre'" class="fixture-team-score">
+      -
+    </span>
+    <span v-else class="fixture-team-score">
+      {{goals}}.{{behinds}}.<span class="fixture-team-score-total">{{totalscore}}</span>
+    </span>
+  </div>
+</template>
+<script lang="ts">
+import IconKit from "../icons/IconKit.vue";
+
+export default {
+    props: {
+      kitsource: {
+        type: String,
+        required: true
+      },
+      gameStatus: {
+        type: String
+      },
+      teamname: {
+        type: String,
+        required: true
+      },
+      goals: {
+        type: Number,
+        required: false
+      },
+      behinds: {
+        type: Number,
+        required: false
+      },
+      totalscore: {
+        type: Number,
+        required: false
+      }
+    },
+    components: { IconKit }
+}
+</script>
+<style scoped lang="scss">
+  .fixture-team {
+    display: inline-block;
+    width: 80px;
+    text-align: center;
+    font-size: 12px;
+    line-height: 16px;
+  }
+  .icon-kit {
+    width: 80px;
+    height: 80px;
+    margin: 12.5px 0;
+    text-align: center;
+  }
+
+  .fixture-team-score {
+    display: block;
+    font-size: 15px;
+    padding-top: 6.25px;
+
+    &-total {
+      font-weight: 600;
+    }
+  }
+</style>
